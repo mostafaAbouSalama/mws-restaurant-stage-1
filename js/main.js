da4e8e3e0900661e27f8124495f2f17fff1ec7ec
Mostafa Adel Abou-Salama <abousalama.mostafa@gmail.com>
2018-08-15 19:22:10 +0200

    Service worker setup

--- js/main.js
+++ js/main.js
@@ -88,18 +88,6 @@ initMap = () => {
 
   updateRestaurants();
 }
-/* window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-} */
 
 /**
  * Update page and map for current restaurants.
@@ -199,13 +187,22 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 }
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
+
+// Registering the service worker
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg) {
+    if (reg.installing) {
+      reg.installing.addEventListener("statechange", function() {
+        if (reg.installing.state == "installed") {
+          console.log("sw successfully installed");
+          return;
+        }
+      });
+    }
+  })
+  .catch(function(error) {
+    console.log("sw installation failed due to " + error);
+    return;
+  })
+}
